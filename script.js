56970c8e5fef9063a4be94dc67f7ffc19ddc97c2
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2022-11-27 18:13:52 -0500

    Added basic JSON support

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function PrintData(){
+    
+    let dataBase = fetch('./Base.json');
+    let data = dataBase.then((response) => response.json());
+    //Access some data \|/
+    data.then((entry) => console.log(entry.e.id))
+    
+}
+
