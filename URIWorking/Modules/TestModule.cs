4d21cefe88cb502d2f78c8766c08fbf2629c4800
KitChaos <97907903+KitChaos-DreamscapeStudios@users.noreply.github.com>
2022-11-28 12:10:11 -0500

    Made test module better

--- URIWorking/Modules/TestModule.cs
+++ URIWorking/Modules/TestModule.cs
@@ -3,12 +3,21 @@ namespace KitDB.TestModule
 {
     class Module
     {
-      public static void Main()
-      {
+        public static void Main()
+        {
+            //Makes an instance of Datagetter
             var Getter = new DataGetter();
+            //Runs the getter to access the Database
             Getter.Run();
-            Console.WriteLine(1 + Int32.Parse(Getter.GetData("id").ToString()));
-      }
+            Console.WriteLine("Enter a Key");
+            //Gets User input
+            var pword = Console.ReadLine();
+            //Accesses the database and prints the associated value
+            Console.WriteLine(Getter.GetData(pword));
+            //A closing input to keep the prompt from closing
+            Console.ReadLine();
+            
+        }
     }
 }
 
